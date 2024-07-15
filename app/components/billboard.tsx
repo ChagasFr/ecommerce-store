a0a912a8972cd9396a025e9c21ce00217273121d
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-14 23:32:03 -0300

    feat: billboards component

---
+++ app/components/billboard.tsx
@@ -0,0 +1,19 @@
+import { Billboard as BillboardType } from "@/types";
+
+interface BillboardProps {
+    data: BillboardType
+};
+
+const Billboard: React.FC<BillboardProps> = ({
+    data
+}) => {
+    return (
+        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Billboard
