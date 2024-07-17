0acbc78be72430aa3d9434aeb29648cd00efee7f
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-17 01:00:43 -0300

    feat: product component

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -2,9 +2,26 @@ import qs from "query-string"
 
 import { Product } from "@/types"
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/bilboard`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
 
-const getProduct = async (id: string): Promise<Product[]> => {
+interface Query {
+    category?: string;
+    colorId?: string;
+    sizeId?: string;
+    isFeatured?: boolean
+}
+
+const getProduct = async (query: Query): Promise<Product[]> => {
+    const url = qs.stringifyUrl({
+        url: URL,
+        query: {
+            colorId: query.colorId,
+            sizeId: query.sizeId,
+            categoryId: query.categoryId,
+            isFeatured: query.isFeatured,
+        }
+    })
+
     const res = await fetch(`${URL}/${id}`);
 
     return res.json()
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import getBillboard from "@/actions/get-categories";
 import Container from "../components/ui/container";
 import Billboard from "../components/billboard";
+import getProducts from "@/actions/get-products";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+
     const billboard = await getBillboard("432879d98-ad32-32a0-342-e473824ff48d0") // passar o correto
 
     return (
