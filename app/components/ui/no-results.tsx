1832ad3b2a76c5ddc9c89b3364a3850ac85657a0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-18 01:02:27 -0300

    feat: ui no-results

--- app/components/ui/no-results.tsx
+++ app/components/ui/no-results.tsx
@@ -1,7 +1,9 @@
 const NoResults = () => {
     return (
-        <div className="flex items-center justify-center">
-
+        <div className="flex items-center justify-center h-full w-full text-neutral-500">
+            no results found
         </div>
     );
 }
+
+export default NoResults;
