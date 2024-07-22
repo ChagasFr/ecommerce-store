d04688c1887a35da2766fac60d973e8004415aba
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-22 15:22:33 -0300

    feat: page of product

--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Product } from "@/types";
 import IconButton from "./icon-button";
 import Currency from "./currency";
+import { useRouter } from "next/router";
 
 interface ProductCard {
     data: Product;
@@ -13,8 +14,14 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/products/${data?.id}`);
+    }
+
     return (
-        <div className=" bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className=" bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images and Actions */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image
