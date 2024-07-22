4018cd21c233f817e2c0b8f3399fac014982a1b8
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-22 16:59:25 -0300

    feat: get product actions

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,4 +1,21 @@
-const ProductPage = () => {
+import getProducts from "@/actions/get-products";
+
+interface ProductRageProps {
+    params: {
+        productId: string;
+    }
+}
+
+
+const ProductPage: React.FC<ProductRageProps> = async ({
+    params
+}) => {
+    const product = {};
+
+    const suggertedProducts = await getProducts({
+        categoryId: product?.category.id
+    })
+
     return (
         <div>
             Individual Page;
