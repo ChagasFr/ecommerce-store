48ebf108be1a751b28107d27e0a8724923abd5be
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-23 09:36:13 -0300

    fix: get product ajust

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,4 +1,7 @@
+import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import ProductList from "@/app/components/product-list";
+import Container from "@/app/components/ui/container";
 
 interface ProductRageProps {
     params: {
@@ -10,15 +13,28 @@ interface ProductRageProps {
 const ProductPage: React.FC<ProductRageProps> = async ({
     params
 }) => {
-    const product = {};
+    const product = await getProduct(params.productId);
 
     const suggertedProducts = await getProducts({
         categoryId: product?.category.id
     })
 
     return (
-        <div>
-            Individual Page;
+        <div className="bg-white">
+            <Container>
+                <div className="pc-4 py-10 sm:px-6 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                        {/* Gallery */}
+                        <div>Gallery</div>
+                        <div className="mt-10 px-4 sm:mt-10 sm:px-0 lg:mt-0">
+                            {/* Info*/}
+                            info
+                        </div>
+                    </div>
+                    <hr className="my-10" />
+                    <ProductList title="Related Items" items={suggertedProducts} />
+                </div>
+            </Container>
         </div>
     );
 }
