4b8588f387e85dbf52d61789a62203ce5886140b
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-25 19:07:54 -0300

    Feat: page routes

---
+++ app/(routes)/product/category/[categoryId]/page.tsx
@@ -0,0 +1,32 @@
+import getProduct from "@/actions/get-products";
+
+export const revalidate = 0;
+
+interface CategoryPageProps {
+    params: {
+        categoryId: string;
+    },
+    searchParams: {
+        colorId: string;
+        sizeId: string
+    }
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+    params,
+    searchParams
+}) => {
+    const products = await getProduct({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    })
+
+    return (
+        <div>
+            Category
+        </div>
+    );
+}
+
+export default CategoryPage;
