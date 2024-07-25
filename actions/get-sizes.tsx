601e8cdb7ac96fd2b1ee72d5847889d6cc1fb70c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-25 19:14:39 -0300

    feat Gets actions

--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -1,8 +1,8 @@
-import { Billboard } from "@/types"
+import { Size } from "@/types"
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
 
-const getSizes = async (id: string): Promise<Billboard[]> => {
+const getSizes = async (id: string): Promise<Size[]> => {
     const res = await fetch(`${URL}/${id}`);
 
     return res.json()
