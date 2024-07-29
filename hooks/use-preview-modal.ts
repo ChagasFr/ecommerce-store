c1ab2ebfcd9db096cbc9cbc73e70f6f30296793a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-29 16:51:41 -0300

    feat: preview modal

---
+++ hooks/use-preview-modal.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+import { Product } from "@/types";
+
+interface PreviewModalStore {
+  isOpen: boolean;
+  data?: Product;
+  onOPen: (data: Product) => void;
+  onClose: () => void;
+}
+
+const usePreviewModalStore = create<PreviewModalStore>((set) => ({
+  isOpen: false,
+  data: undefined,
+  onOpen: (data: Product) => set({ data, isOpen: true }),
+  onCLose: () => set({ isOpen: false }),
+}));
+
+export default usePreviewModal;
