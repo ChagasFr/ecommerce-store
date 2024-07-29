67ff962e2853ceb996e94b931ecb44a048f82e91
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-28 23:37:53 -0300

    feat: mobile filter

--- app/(routes)/product/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/product/category/[categoryId]/components/mobile-filters.tsx
@@ -1,6 +1,22 @@
 "use client";
 
-const MobileFilters = () => {
+import { Color, Size } from "@/types";
+import { useState } from "react";
+
+interface MobileFiltersProps {
+    sizes: Size[];
+    colors: Color[];
+};
+
+const MobileFilters: React.FC<MobileFiltersProps> = ({
+    sizes,
+    colors
+}) => {
+    const [open, setOpen] = useState(false);
+
+    const onOpen = () => setOpen(true);
+    const onClose = () => setOpen(false);
+
     return (
         <div>
             MobileFilters
