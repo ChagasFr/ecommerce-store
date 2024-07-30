ef272f214fff0c25d266ba4a242465d536d6fed6
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-30 14:05:37 -0300

    Feat: preview modal

--- app/components/preview-modal.tsx
+++ app/components/preview-modal.tsx
@@ -3,6 +3,7 @@
 import usePreviewModal from "@/hooks/use-preview-modal";
 import Modal from "./ui/modal";
 import Gallery from "./gallery";
+import Info from "./info";
 
 const PreviewModel = () => {
     const previewModal = usePreviewModal();
@@ -20,6 +21,9 @@ const PreviewModel = () => {
             <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
                 <div className="sm:col-span-4 lg:col-span-5">
                     <Gallery images={product.images} />
+                </div>
+                <div className="sm:col-sapn-8 lg:col-span-7">
+                    <Info data={product} />
                 </div>
             </div>
 
